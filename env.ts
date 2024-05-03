59452eedb54e20d3591adfc47f0d9d615c6aacce
Bastien Genin <bastien.genin@jdc.fr>
2024-05-03 14:17:19 +0200

    feat (.workflows,/,Index.ts): undefined

--- env.ts
+++ env.ts
@@ -2,14 +2,14 @@ import { createEnv } from '@t3-oss/env-core'
 import { z } from 'zod'
 
 export const env = createEnv({
-	runtimeEnv: process.env,
-	/*
-	 * ServerSide Environment variables, not available on the client.
-	 * Will throw if you access these variables on the client.
-	 */
-	server: {
-		SITE_URL: z.string().url(),
-		THEO_USER_NAME: z.string().min(1),
-		THEO_USER_PASSWORD: z.string().min(1),
-	},
+  runtimeEnv: process.env,
+  /*
+   * ServerSide Environment variables, not available on the client.
+   * Will throw if you access these variables on the client.
+   */
+  server: {
+    SITE_URL: z.string().url(),
+    USER_NAME: z.string().min(1),
+    USER_PASSWORD: z.string().min(1)
+  }
 })
